50f446686840e6ac9909b15fb65d7b88addd81d3
kyeongrok <hydena03@gmail.com>
2024-04-08 23:46:09 +0900

    04082346

--- src/layout/pages/Homepage/Homepage.jsx
+++ src/layout/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Banner from './component/Banner/Banner'
+import PopularMovieSlide from './component/PopularMovieSlide/PopularMovieSlide'
 
 //1.배너 => popular 영화의 첫번째 아이템
 //2. popular movie
@@ -10,6 +11,7 @@ const Homepage = () => {
   return (
     <div>
         <Banner />
+        <PopularMovieSlide/>
     </div>
   )
 }
--- src/layout/pages/Homepage/component/Banner/Banner.jsx
+++ src/layout/pages/Homepage/component/Banner/Banner.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { usePopularMoviesQuery } from '../../../../../hooks/usePopularMovies';
 import Alert from 'react-bootstrap/Alert';
-import"./Banner.style.css"
+import "./Banner.style.css";
+
 const Banner = () => {
     const { data, isLoading, isError, error } = usePopularMoviesQuery();
     console.log("ddd, data");
@@ -15,15 +16,16 @@ const Banner = () => {
     }
 
     return (
-        <div style={{
-            backgroundImage: `url(https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path})`
-        }}
-        className="banner"
+        <div
+            style={{
+                backgroundImage: `url(https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path})`
+            }}
+            className="banner"
         >
-       {/* <div className="text-white banner-text-area">
-        <h1>{data.result[0].title}</h1>
-        <p>{data.result[0].overview}</p>
-       </div> */}
+            <div className="text-white banner-text-area">
+                <h1>{data.results[0].title}</h1>
+                <p>{data.results[0].overview}</p>
+            </div>
         </div>
     );
 }
